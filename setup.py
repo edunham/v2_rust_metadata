84f718c693db78880438d43303cc9dafbcbad235
edunham <edunham@mozilla.com>
2015-10-29 16:27:01 -0700

    Depends on argparse

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=['argparse==1.2.1'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
